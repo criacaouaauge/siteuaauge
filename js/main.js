8862d6c22dbc1fa09f6a633418c3f47f168f1e79
Gustavo Sperandio <gusperandio2@gmail.com>
2021-06-28 16:28:50 -0300

    Update and test of php navbar

--- js/main.js
+++ js/main.js
@@ -1365,3 +1365,17 @@ function move_gradient() {
   	$('.picture').removeClass('active');
   })
 }
+var btn = $('#button-top');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
