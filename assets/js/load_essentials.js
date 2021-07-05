a3f3d0d11b1d561093114293d67126af6ba8e2e1
Guilherme Santos <guilherme.santos@sap.com>
2021-07-05 12:22:00 +0100

    minor fix

--- assets/js/load_essentials.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("myFooter").innerHTML =
-	"<p id='copyright'>Copyright &copy; " + new Date().getFullYear() + " You. All"
-	+ " rights reserved.</p>"
-	+ "<p id='credits'>Layout by You</p>"
-	+ "<p id='contact'><a href='mailto:you@you.com'>Contact Us</a> / "
-	+ "<a href='mailto:you@you.com'>Report a problem.</a></p>";
